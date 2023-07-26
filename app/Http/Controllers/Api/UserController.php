3fb4649c122021d424fcaa467513cbd597edbbe1
Moch Azmi Iskandar <azmiiskandar0@gmail.com>
2023-07-26 21:19:29 +0700

    🔀 chore(UserController.php): move successResponse and errorResponse methods to ApiResponseTrait for code organization and reusability 🎉 feat(ApiResponseTrait.php): add ApiResponseTrait to provide common functionality for handling API responses

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,11 +4,21 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Services\User\UserService;
+use App\Traits\ApiResponseTrait;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
 class UserController extends Controller
 {
+    /**
+     * This trait provides common functionality for handling API responses.
+     */
+    use ApiResponseTrait;
+
+    /**
+     * The instance of the UserService class.
+     * @var UserService
+     */
     protected $userService;
 
     /**
@@ -69,44 +79,5 @@ class UserController extends Controller
 
         // Return both 'sort' and 'limit' values.
         return [$sort, $limit];
-    }
-
-    /**
-     * Prepare success response.
-     * @param  string $message
-     * @param  mixed $data
-     * @return \Illuminate\Http\Response
-     */
-    protected function successResponse(string $message, $data)
-    {
-        // Prepare response array with standard success status, HTTP 200 code, custom message, and the data to return.
-        $response = [
-            'status' => 'success',
-            'code' => 200,
-            'message' => $message,
-            'results' => $data,
-        ];
-
-        // Convert the response array into a JSON response with HTTP 200 status code.
-        return response()->json($response, 200);
-    }
-
-    /**
-     * Prepare error response.
-     * @param  string $message
-     * @param  int $code
-     * @return \Illuminate\Http\Response
-     */
-    protected function errorResponse(string $message, int $code)
-    {
-        // Prepare response array with standard error status, custom HTTP code and custom error message.
-        $response = [
-            'status' => 'error',
-            'code' => $code,
-            'message' => $message,
-        ];
-
-        // Convert the response array into a JSON response with custom HTTP status code.
-        return response()->json($response, $code);
     }
 }
